20bebbd0d3b441947cb7bee56355432b681b9ba5
msi1995 <63132397+msi1995@users.noreply.github.com>
2021-03-07 20:58:49 -0800

    first leap year test

--- HW7/test_leapyear.py
+++ HW7/test_leapyear.py
@@ -1,1 +1,8 @@
+import unittest
+import leapyear2 as leap
 
+class TestLeap(unittest.TestCase):
+
+    def test_leap2100(self):
+        check = leap.checkleap(2100)
+        self.assertEqual(check, True) 
