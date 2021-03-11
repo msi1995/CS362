c69a1f94ce627faab43e3607d4746e2576d86b97
msi1995 <63132397+msi1995@users.noreply.github.com>
2021-03-10 17:48:02 -0800

    Update test_fibonacci.py

--- in-class-2-25/test_fibonacci.py
+++ in-class-2-25/test_fibonacci.py
@@ -3,16 +3,16 @@ import fibonacci as fib
 
 class TestFib(unittest.TestCase):
     def test_fib8(self):
-        w = fib.fibonacci(8)
-        self.assertEqual(w, 21)
+        check = fib.fibonacci(8)
+        self.assertEqual(check, 21)
 
     def test_fib3(self):
-        w = fib.fibonacci(3)
-        self.assertEqual(w, 2)
+        check = fib.fibonacci(3)
+        self.assertEqual(check, 2)
 
     def test_fib7(self):
-        w = fib.fibonacci(7)
-        self.assertEqual(w, 13)
+        check = fib.fibonacci(7)
+        self.assertEqual(check, 13)
 
 def test_fib():
     assert fib.fibonacci(8) == 21
